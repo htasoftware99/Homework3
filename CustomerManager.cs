3fb43a3f35ee573870336a290a6d6ef4ba633457
htasoftware99 <hasan_tugra1999@outlook.com>
2023-04-19 13:26:35 +0300

    KampIntoHomework

---
+++ CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer is added: " + customer.Name);
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer is deleted: " + customer.Name);
+        }
+
+        public void ListCustomer(Customer[] customers)
+        {
+            Console.WriteLine("The number of customers: ", customers.Length);
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("Id: " + customer.Id);
+                Console.WriteLine("Name: " + customer.Name);
+                Console.WriteLine("Surname: " + customer.Surname);
+                Console.WriteLine("City: " + customer.City);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using ClassMetotDemo;
+
+Customer customer1 = new Customer();
+customer1.Id = 1;
+customer1.Name = "Hasan";
+customer1.Surname = "Aykaç";
+customer1.City = "Ankara";
+
+Customer customer2 = new Customer();
+customer2.Id = 2;
+customer2.Name = "Mehmet";
+customer2.Surname = "Buğra";
+customer2.City = "Çorum";
+
+CustomerManager customerManager =  new CustomerManager();
+customerManager.Add(customer1);
+customerManager.Add(customer2);
+customerManager.Delete(customer1);
+customerManager.Delete(customer2);
+
+Customer[] customer = new Customer[] { customer1, customer2 };
+customerManager.ListCustomer(customer);
